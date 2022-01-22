6bd5fe91f9d5ec4498feceac56501178620991a2
santhoshkmr-official <96934265+santhoshkmr-official@users.noreply.github.com>
2022-01-22 21:07:31 +0530

    Create  Peer-graded Assignment: Print a poem.c

---
+++ Coding for Everyone: C and C++ Specialization/C for Everyone: Programming Fundamentals/Week 1/Peer-graded Assignment: Print a poem.c
@@ -0,0 +1,13 @@
+/*
+  Written by: Santhosh K.M.R
+  Written on: 22/01/2022
+*/
+
+#include<stdio.h>
+
+int main(void)
+  
+{   
+    printf("The plates will still shift\n and the clouds will still spew\n The sun will slowly rise\n and the moon will follow too");
+    return 0;
+}    
