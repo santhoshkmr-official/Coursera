fdecfd5f8901194cc056d1bc888a1367ef828dad
santhoshkmr-official <96934265+santhoshkmr-official@users.noreply.github.com>
2022-01-22 21:03:48 +0530

    Create Peer-graded Assignment: Fix Dr. P’s mistake.c

---
+++ Coding for Everyone: C and C++ Specialization/C for Everyone: Programming Fundamentals/Week 1/Peer-graded Assignment: Fix Dr. P’s mistake.c
@@ -0,0 +1,13 @@
+/*
+  Written by: Santhosh K.M.R
+  Written on: 22/01/2022
+*/
+
+#include<stdio.h>
+
+int main(void)
+  
+{   
+    printf(" hello world\n"); //Replaced the single quotes with double quotes to fix the syntax error
+    return 0;
+}    
